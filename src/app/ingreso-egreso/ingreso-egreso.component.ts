4cf076c238eef0353e24cc80c16311e9ad2ec84e
DavichoDev <gamers_wow@hotmail.com>
2021-02-07 18:47:21 -0600

    Primer commit

---
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ingreso-egreso',
+  templateUrl: './ingreso-egreso.component.html',
+  styles: [
+  ]
+})
+export class IngresoEgresoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
